8ef9e2f0e3b0883f42a32df7908171b8d8f3ef55
MoussaOudjama <moussa.oudjama@canal-plus.com>
2021-06-29 15:59:30 +0200

    init project

--- SakoutcherMqttTester/HomeViewController.swift
+++ SakoutcherMqttTester/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  SakoutcherMqttTester
 //
 //  Created by Oudjama on 29/06/2021.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
-
 }
-
